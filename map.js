3b5df0385ffe64da2d649dc988000cc67dc266f8
mj2960 <61558010+mj2960@users.noreply.github.com>
2020-02-28 10:56:32 -0500

    Add files via upload

---
+++ map.js
@@ -0,0 +1,7 @@
+'use strict'        // let the browser know we're serious
+
+// debug statement letting us know the file is loaded
+console.log('Loaded map.js')
+
+// your mapbox token
+mapboxgl.accessToken = 'pk.eyJ1IjoibWoyOTYwIiwiYSI6ImNrNzU4eXkwNjB2eHkzbXBuMmxpeng4ZWEifQ.HCra4TqNRHHkpKDmYRAK1w'
