17e5c37a5a8ae4aa90df83f1d12e0d150e4c325b
mj2960 <61558010+mj2960@users.noreply.github.com>
2020-03-01 12:10:00 -0500

    Update map.js

--- map.js
+++ map.js
@@ -5,3 +5,51 @@ console.log('Loaded map.js')
 
 // your mapbox token
 mapboxgl.accessToken = 'pk.eyJ1IjoibWoyOTYwIiwiYSI6ImNrNzU4eXkwNjB2eHkzbXBuMmxpeng4ZWEifQ.HCra4TqNRHHkpKDmYRAK1w'
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mj2960/ck76exelt1pcu1ipc5kh0go7p',
+    center: [-89.8113, 30.0221],
+    zoom: 10
+    // center: [-73.93324, 40.80877],
+    // zoom: 14
+});
+var trees_url = "./data/nyc_harlemriverstreettrees_4326.geojson"
+
+
+map.on('load',function(){
+
+        // NYPL map warper raster tile service as source
+map.addSource('nypl',{
+    'type':'raster',
+    'tiles':["http://maps.nypl.org/warper/maps/tile/27677/{z}/{x}/{y}.png "],
+    "tileSize": 256,
+  });
+  // create a layer from the NYPL tiles
+  map.addLayer({
+    'id':'nypl-m',
+    'type':'raster',
+    'source':'nypl',
+    'paint':{
+      'raster-opacity':0.85
+    }
+})
+
+    // define a 'source' for your point dataset
+    map.addSource('trees_data',{
+      'type':'geojson',
+      'data': "./data/nyc_harlemriverstreettrees_4326.geojson"
+    });
+    // add a new layer with your points
+    map.addLayer({
+      'id':'trees',
+      'type':'circle',
+      'source':'trees_data',
+      'paint':{
+        'circle-radius':4,
+        'circle-color': '#349f27',
+        'circle-opacity':0.7
+      },
+    })
+  
+  
+  });
